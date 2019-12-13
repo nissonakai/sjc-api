e9c2e60039e4694164858a167d3055dd88a34c83
s-nakai <s-nakai@nisso.co.jp>
2019-12-13 14:41:24 +0900

    add origins

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module App
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "localhost:3000", "https://suitable-job-checker-nisso.netlify.com/"
+        origins "https://suitable-job-checker-nisso.netlify.com/", "localhost:3000"
         resource "*",
           headers: :any,
           methods: [:get, :post, :put, :delete, :options, :head]
