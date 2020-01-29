9043edfcd47156cea8dd4e9a93635f733bc9128c
s-nakai <s-nakai@nisso.co.jp>
2020-01-29 17:34:08 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 # coding: utf-8
 
-Question.create(title: "title1", red: "test1", blue: "test2")
-Question.create(title: "title2", red: "test3", blue: "test4")
-Question.create(title: "title3", red: "test5", blue: "test6")
-Question.create(title: "title4", red: "test7", blue: "test8")
+Survey.create(name: "テストアンケート1")
+Survey.create(name: "テストアンケート2")
+Survey.create(name: "テストアンケート3")
+Question.create(title: "title1", red: "test1", blue: "test2", servey_id: 1)
+Question.create(title: "title2", red: "test3", blue: "test4", servey_id: 1)
+Question.create(title: "title3", red: "test5", blue: "test6", servey_id: 1)
+Question.create(title: "title4", red: "test7", blue: "test8", servey_id: 1)
